e69d9217ac3b0e2cf720996407679c6c64798c8f
groinking <dungtran@var-meta.com>
2026-02-10 13:36:34 +0700

    chore: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
-import logoUrl from "./assets/herald-bg.jpg";
+import HeraldPage from "./pages/herald";
 
 function App() {
 	return (
-		<div>
-			<img
-				src={logoUrl}
-				alt="logo"
-				className="w-screen h-screen object-cover"
-			/>
-			<div className="absolute top-0 left-0 right-0 bottom-0 m-auto w-full bg-black/30 flex items-center justify-center">
-				<h1 className="text-white text-4xl font-bold">Herald AI</h1>
-			</div>
-		</div>
+		<Routes>
+			<Route path="/" element={<HeraldPage />} />
+			<Route path="*" element={<HeraldPage />} />
+		</Routes>
 	);
 }
 
---
+++ src/pages/herald.tsx
@@ -0,0 +1,16 @@
+import logoUrl from "../assets/herald-bg.jpg";
+
+export default function HeraldPage() {
+	return (
+		<div>
+			<img
+				src={logoUrl}
+				alt="logo"
+				className="w-screen h-screen object-cover"
+			/>
+			<div className="absolute top-0 left-0 right-0 bottom-0 m-auto w-full bg-black/30 flex items-center justify-center">
+				<h1 className="text-white text-4xl font-bold">Herald AI</h1>
+			</div>
+		</div>
+	);
+}
